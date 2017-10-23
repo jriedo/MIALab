16cd37bb0cd734d513d1e14b4b9c6d87b40a8350
riedj1 <jan_riedo@hotmail.com>
2017-10-23 14:16:20 +0200

    DF crossvalidation implementation

--- bin/df_crossval_main.py
+++ bin/df_crossval_main.py
@@ -10,8 +10,7 @@ import timeit
 
 import SimpleITK as sitk
 import numpy as np
-from tensorflow.python.platform import app
-from sklearn.neighbors import KNeighborsClassifier
+
 
 sys.path.insert(0, os.path.join(os.path.dirname(sys.argv[0]), '..'))  # append the MIALab root directory to Python path
 # fixes the ModuleNotFoundError when executing main.py in the console after code changes (e.g. git pull)
@@ -23,13 +22,15 @@ import mialab.data.structure as structure
 import mialab.data.loading as load
 import mialab.utilities.file_access_utilities as futil
 import mialab.utilities.pipeline_utilities as putil
+import mialab.utilities.statistic_utilities as statistics
 
 FLAGS = None  # the program flags
 IMAGE_KEYS = [structure.BrainImageTypes.T1, structure.BrainImageTypes.T2, structure.BrainImageTypes.GroundTruth]  # the list of images we will load
-TEST_BATCH_SIZE = 1  # 1..30, the higher the faster but more memory usage
+TRAIN_BATCH_SIZE = 70  # 1..70, the higher the faster but more memory usage
+TEST_BATCH_SIZE = 2  # 1..30, the higher the faster but more memory usage
 
+def main(FLAGS,trees,nodes):
 
-def main(_):
     """Brain tissue segmentation using decision forests.
 
     The main routine executes the medical image analysis pipeline:
@@ -52,6 +53,7 @@ def main(_):
     # generate a model directory (use datetime to ensure that the directory is empty)
     # we need an empty directory because TensorFlow will continue training an existing model if it is not empty
     t = datetime.datetime.now().strftime('%Y-%m-%d%H%M%S')
+    t='DF_trees_'+str(trees)+'_nodes_'+str(nodes)
     model_dir = os.path.join(FLAGS.model_dir, t)
     os.makedirs(model_dir, exist_ok=True)
 
@@ -62,28 +64,44 @@ def main(_):
                                          futil.DataDirectoryFilter())
     data_items = list(crawler.data.items())
 
-    pre_process_params = {'zscore_pre': True,#1 features
-                          'coordinates_feature': False,#3 features
-                          'intensity_feature': True,#1 features
-                          'gradient_intensity_feature': True}#2 features
+    pre_process_params = {'zscore_pre': True,
+                          'coordinates_feature': True,
+                          'intensity_feature': True,
+                          'gradient_intensity_feature': True}
 
-    n_neighbors=1
+    # initialize decision forest parameters
+    df_params = df.DecisionForestParameters()
+    df_params.num_classes = 4
+    df_params.num_trees = trees
+    df_params.max_nodes = nodes
+    df_params.model_dir = model_dir
+    forest = None
+    start_time_total_train = timeit.default_timer()
 
-    batch_data = dict(data_items)
-    # load images for training and pre-process
-    images = putil.pre_process_batch(batch_data, pre_process_params, multi_process=True)
-    print('pre-processing done')
+    for batch_index in range(0, len(data_items), TRAIN_BATCH_SIZE):
 
-    # generate feature matrix and label vector
-    data_train = np.concatenate([img.feature_matrix[0] for img in images])
-    labels_train = np.concatenate([img.feature_matrix[1] for img in images])
 
-    start_time = timeit.default_timer()
-    # forest.train(data_train, labels_train)
-    neigh = KNeighborsClassifier(n_neighbors=n_neighbors,weights='distance',algorithm='auto')#weights=’uniform’
-    knn_fit=neigh.fit(data_train, labels_train[:,0])
-    print(' Time elapsed:', timeit.default_timer() - start_time, 's')
 
+        # slicing manages out of range; no need to worry
+        batch_data = dict(data_items[batch_index: batch_index+TRAIN_BATCH_SIZE])
+        # load images for training and pre-process
+        images = putil.pre_process_batch(batch_data, pre_process_params, multi_process=True)
+        print('pre-processing done')
+
+        # generate feature matrix and label vector
+        data_train = np.concatenate([img.feature_matrix[0] for img in images])
+        labels_train = np.concatenate([img.feature_matrix[1] for img in images])
+
+        if forest is None:
+            df_params.num_features = data_train.shape[1]
+            print(df_params)
+            forest = df.DecisionForest(df_params)
+
+        start_time = timeit.default_timer()
+        forest.train(data_train, labels_train)
+        print(' Time elapsed:', timeit.default_timer() - start_time, 's')
+
+    time_total_train = timeit.default_timer() - start_time_total_train
     print('-' * 5, 'Testing...')
     result_dir = os.path.join(FLAGS.result_dir, t)
     os.makedirs(result_dir, exist_ok=True)
@@ -113,15 +131,12 @@ def main(_):
             print('-' * 10, 'Testing', img.id_)
 
             start_time = timeit.default_timer()
-            # probabilities, predictions = forest.predict(img.feature_matrix[0])
-            predictions=neigh.predict(img.feature_matrix[0])
-            probabilities=neigh.predict_proba(img.feature_matrix[0])
+            probabilities, predictions = forest.predict(img.feature_matrix[0])
             print(' Time elapsed:', timeit.default_timer() - start_time, 's')
 
             # convert prediction and probabilities back to SimpleITK images
             image_prediction = conversion.NumpySimpleITKImageBridge.convert(predictions.astype(np.uint8),
                                                                             img.image_properties)
-
             image_probabilities = conversion.NumpySimpleITKImageBridge.convert(probabilities, img.image_properties)
 
             # evaluate segmentation without post-processing
@@ -144,46 +159,14 @@ def main(_):
             sitk.WriteImage(images_post_processed[i], os.path.join(result_dir, images_test[i].id_ + '_SEG-PP.mha'), True)
 
 
-if __name__ == "__main__":
-    """The program's entry point."""
+    # write summary of parameters to results dir
+    with open(os.path.join(result_dir, 'summary.txt'), 'w') as summary_file:
+        print('Training data size: {}'.format(len(data_items)), file=summary_file)
+        print('Total training time: {:.1f}s'.format(time_total_train), file=summary_file)
+        print('Decision forest', file=summary_file)
+        print(df_params, file=summary_file)
+        stats = statistics.gather_statistics(os.path.join(result_dir, 'results.csv'))
+        print('Result statistics:', file=summary_file)
+        print(stats, file=summary_file)
 
-    script_dir = os.path.dirname(sys.argv[0])
 
-    parser = argparse.ArgumentParser(description='Medical image analysis pipeline for brain tissue segmentation')
-    parser.add_argument(
-        '--model_dir',
-        type=str,
-        default=os.path.normpath(os.path.join(script_dir, './mia-model')),
-        help='Base directory for output models.'
-    )
-
-    parser.add_argument(
-        '--result_dir',
-        type=str,
-        default=os.path.normpath(os.path.join(script_dir, './mia-result')),
-        help='Directory for results.'
-    )
-
-    parser.add_argument(
-        '--data_atlas_dir',
-        type=str,
-        default=os.path.normpath(os.path.join(script_dir, '../data/atlas')),
-        help='Directory with atlas data.'
-    )
-
-    parser.add_argument(
-        '--data_train_dir',
-        type=str,
-        default=os.path.normpath(os.path.join(script_dir, '../data/train/')),
-        help='Directory with training data.'
-    )
-
-    parser.add_argument(
-        '--data_test_dir',
-        type=str,
-        default=os.path.normpath(os.path.join(script_dir, '../data/test/')),
-        help='Directory with testing data.'
-    )
-
-    FLAGS, unparsed = parser.parse_known_args()
-    app.run(main=main, argv=[sys.argv[0]] + unparsed)
