83f5329636a47446f59a137a7df0c5e2ed7893b6
Michael Müller <mlm2@bfh.ch>
2017-10-25 09:10:50 +0200

    Added scatter_plot, feature_evaluation, optimized main_GMM

--- bin/feature_evaluation.py
+++ bin/feature_evaluation.py
@@ -8,10 +8,11 @@ import os
 import sys
 import timeit
 
+
 import SimpleITK as sitk
 import numpy as np
 from tensorflow.python.platform import app
-from sklearn.mixture import GaussianMixture
+
 
 sys.path.insert(0, os.path.join(os.path.dirname(sys.argv[0]), '..'))  # append the MIALab root directory to Python path
 # fixes the ModuleNotFoundError when executing main.py in the console after code changes (e.g. git pull)
@@ -23,11 +24,13 @@ import mialab.data.structure as structure
 import mialab.data.loading as load
 import mialab.utilities.file_access_utilities as futil
 import mialab.utilities.pipeline_utilities as putil
+import mialab.utilities.statistic_utilities as statistics
 
 FLAGS = None  # the program flags
 IMAGE_KEYS = [structure.BrainImageTypes.T1, structure.BrainImageTypes.T2, structure.BrainImageTypes.GroundTruth]  # the list of images we will load
-TEST_BATCH_SIZE = 10  # 1..30, the higher the faster but more memory usage
-
+TRAIN_BATCH_SIZE = 5  # 1..70, the higher the faster but more memory usage
+TEST_BATCH_SIZE = 5  # 1..30, the higher the faster but more memory usage
+USE_PREPROCESS_CACHE = False    # cache pre-processed images
 
 def main(_):
     """Brain tissue segmentation using decision forests.
@@ -67,28 +70,60 @@ def main(_):
                           'intensity_feature': True,
                           'gradient_intensity_feature': True}
 
-    batch_data = dict(data_items)
-    # load images for training and pre-process
-    images = putil.pre_process_batch(batch_data, pre_process_params, multi_process=True)
-    print('pre-processing done')
+    # initialize decision forest parameters
+    df_params = df.DecisionForestParameters()
+    df_params.num_classes = 4
+    df_params.num_trees = 3
+    df_params.max_nodes = 2000
+    df_params.model_dir = model_dir
+    forest = None
+    start_time_total_train = timeit.default_timer()
 
-    # generate feature matrix and label vector
-    data_train = np.concatenate([img.feature_matrix[0] for img in images])
-    labels_train = np.concatenate([img.feature_matrix[1] for img in images])
+    for batch_index in range(0, len(data_items), TRAIN_BATCH_SIZE):
+        cache_file_prefix = os.path.normpath(os.path.join(script_dir, './mia-cache/batch-' + str(batch_index) + '-' + str(TRAIN_BATCH_SIZE)))
+        cache_file_train = cache_file_prefix + '-data_train.npy'
+        cache_file_labels = cache_file_prefix + '-data_labels.npy'
+        if(USE_PREPROCESS_CACHE & os.path.exists(cache_file_train)):
+            print('Using cache from ', cache_file_train)
+            data_train = np.load(cache_file_train)
+            labels_train = np.load(cache_file_labels)
+        else:
+            # slicing manages out of range; no need to worry
+            batch_data = dict(data_items[batch_index: batch_index+TRAIN_BATCH_SIZE])
+            # load images for training and pre-process
+            images = putil.pre_process_batch(batch_data, pre_process_params, multi_process=True)
+            print('pre-processing done')
 
-    start_time = timeit.default_timer()
-    # Gaussian mixture model
-    # ##############################################################################################################
+            # generate feature matrix and label vector
+            data_train = np.concatenate([img.feature_matrix[0] for img in images])
+            labels_train = np.concatenate([img.feature_matrix[1] for img in images])
+
+            if(USE_PREPROCESS_CACHE):
+                print('Writing cache')
+                if(not os.path.exists(os.path.dirname(cache_file_prefix))):
+                    os.mkdir(os.path.dirname(cache_file_prefix))
+                data_train.dump(cache_file_train)
+                labels_train.dump(cache_file_labels)
 
 
+        if forest is None:
+            df_params.num_features = data_train.shape[1]
+            forest = df.DecisionForest(df_params)
 
+        start_time = timeit.default_timer()
+        # ##############################################################################################################
+        print(data_train)
+        print(np.shape(data_train))
 
+        data_train = np.delete(data_train, (0,1,2,3,4,5), 1)
 
-    thegmm = GaussianMixture(n_components=3,  covariance_type='tied')
-    thegmm.fit(data_train, labels_train)
-    # ##############################################################################################################
-    print(' Time elapsed:', timeit.default_timer() - start_time, 's')
+        print(data_train)
+        print(np.shape(data_train))
+        # ##############################################################################################################
+        forest.train(data_train, labels_train)
+        print(' Time elapsed:', timeit.default_timer() - start_time, 's')
 
+    time_total_train = timeit.default_timer() - start_time_total_train
     print('-' * 5, 'Testing...')
     result_dir = os.path.join(FLAGS.result_dir, t)
     os.makedirs(result_dir, exist_ok=True)
@@ -116,17 +151,14 @@ def main(_):
 
         for img in images_test:
             print('-' * 10, 'Testing', img.id_)
+
             start_time = timeit.default_timer()
-            # ##############################################################################################################
-            predictions = thegmm.predict(img.feature_matrix[0])
-            probabilities = thegmm.predict_proba(img.feature_matrix[0])
-            # ##############################################################################################################
+            probabilities, predictions = forest.predict(img.feature_matrix[0])
             print(' Time elapsed:', timeit.default_timer() - start_time, 's')
 
             # convert prediction and probabilities back to SimpleITK images
             image_prediction = conversion.NumpySimpleITKImageBridge.convert(predictions.astype(np.uint8),
                                                                             img.image_properties)
-
             image_probabilities = conversion.NumpySimpleITKImageBridge.convert(probabilities, img.image_properties)
 
             # evaluate segmentation without post-processing
@@ -135,7 +167,7 @@ def main(_):
             images_prediction.append(image_prediction)
             images_probabilities.append(image_probabilities)
 
-            # post-process segmentation and evaluate with post-processing
+        # post-process segmentation and evaluate with post-processing
         post_process_params = {'crf_post': True}
         images_post_processed = putil.post_process_batch(images_test, images_prediction, images_probabilities,
                                                          post_process_params, multi_process=True)
@@ -147,6 +179,17 @@ def main(_):
             # save results
             sitk.WriteImage(images_prediction[i], os.path.join(result_dir, images_test[i].id_ + '_SEG.mha'), True)
             sitk.WriteImage(images_post_processed[i], os.path.join(result_dir, images_test[i].id_ + '_SEG-PP.mha'), True)
+
+
+    # write summary of parameters to results dir
+    with open(os.path.join(result_dir, 'summary.txt'), 'w') as summary_file:
+        print('Training data size: {}'.format(len(data_items)), file=summary_file)
+        print('Total training time: {:.1f}s'.format(time_total_train), file=summary_file)
+        print('Decision forest', file=summary_file)
+        print(df_params, file=summary_file)
+        stats = statistics.gather_statistics(os.path.join(result_dir, 'results.csv'))
+        print('Result statistics:', file=summary_file)
+        print(stats, file=summary_file)
 
 
 if __name__ == "__main__":
---
+++ bin/scatter_plot.py
@@ -0,0 +1,125 @@
+"""A medical image analysis pipeline.
+
+The pipeline is used for brain tissue segmentation using a decision forest classifier.
+"""
+import argparse
+import datetime
+import os
+import sys
+
+import numpy as np
+from tensorflow.python.platform import app
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+sys.path.insert(0, os.path.join(os.path.dirname(sys.argv[0]), '..'))  # append the MIALab root directory to Python path
+# fixes the ModuleNotFoundError when executing main.py in the console after code changes (e.g. git pull)
+# somehow pip install does not keep track of packages
+
+import mialab.data.structure as structure
+import mialab.data.loading as load
+import mialab.utilities.file_access_utilities as futil
+import mialab.utilities.pipeline_utilities as putil
+
+FLAGS = None  # the program flags
+IMAGE_KEYS = [structure.BrainImageTypes.T1, structure.BrainImageTypes.T2, structure.BrainImageTypes.GroundTruth]  # the list of images we will load
+TRAIN_BATCH_SIZE = 5  # 1..70, the higher the faster but more memory usage
+TEST_BATCH_SIZE = 5  # 1..30, the higher the faster but more memory usage
+USE_PREPROCESS_CACHE = False    # cache pre-processed images
+
+
+def main(_):
+    # generate a model directory (use datetime to ensure that the directory is empty)
+    # we need an empty directory because TensorFlow will continue training an existing model if it is not empty
+    t = datetime.datetime.now().strftime('%Y-%m-%d%H%M%S')
+    model_dir = os.path.join(FLAGS.model_dir, t)
+    os.makedirs(model_dir, exist_ok=True)
+
+    # crawl the training image directories
+    crawler = load.FileSystemDataCrawler(FLAGS.data_train_dir,
+                                         IMAGE_KEYS,
+                                         futil.BrainImageFilePathGenerator(),
+                                         futil.DataDirectoryFilter())
+    data_items = list(crawler.data.items())
+
+    pre_process_params = {'zscore_pre': True,
+                          'coordinates_feature': True,
+                          'intensity_feature': True,
+                          'gradient_intensity_feature': True}
+
+    for batch_index in range(0, len(data_items), TRAIN_BATCH_SIZE):
+        cache_file_prefix = os.path.normpath(os.path.join(script_dir, './mia-cache/batch-' + str(batch_index) + '-' + str(TRAIN_BATCH_SIZE)))
+        cache_file_train = cache_file_prefix + '-data_train.npy'
+        cache_file_labels = cache_file_prefix + '-data_labels.npy'
+        if(USE_PREPROCESS_CACHE & os.path.exists(cache_file_train)):
+            print('Using cache from ', cache_file_train)
+            data_train = np.load(cache_file_train)
+            labels_train = np.load(cache_file_labels)
+        else:
+            # slicing manages out of range; no need to worry
+            batch_data = dict(data_items[batch_index: batch_index+TRAIN_BATCH_SIZE])
+            # load images for training and pre-process
+            images = putil.pre_process_batch(batch_data, pre_process_params, multi_process=True)
+
+            # generate feature matrix and label vector
+            data_train = np.concatenate([img.feature_matrix[0] for img in images])
+            labels_train = np.concatenate([img.feature_matrix[1] for img in images])
+
+
+    # Scatter matrix plot of the train data
+
+    data = pd.DataFrame(data_train, columns=['Feature 1', 'Feature 2', 'Feature 3', 'Feature 4', 'Feature 5',
+                                                                                    'Feature 6', 'Feature 7'])
+    print(data)
+    # data.drop([1,2], axis=1)
+    # print(data)
+    axes = pd.scatter_matrix(data, alpha=0.2, diagonal='hist')
+    corr = data.corr().as_matrix()
+    for i, j in zip(*plt.np.triu_indices_from(axes, k=1)):
+        axes[i, j].annotate("%.3f" % corr[i, j], (0.85, 0.85), xycoords='axes fraction', ha='center', va='center')
+    plt.show()
+
+if __name__ == "__main__":
+    """The program's entry point."""
+
+    script_dir = os.path.dirname(sys.argv[0])
+
+    parser = argparse.ArgumentParser(description='Medical image analysis pipeline for brain tissue segmentation')
+    parser.add_argument(
+        '--model_dir',
+        type=str,
+        default=os.path.normpath(os.path.join(script_dir, './mia-model')),
+        help='Base directory for output models.'
+    )
+
+    parser.add_argument(
+        '--result_dir',
+        type=str,
+        default=os.path.normpath(os.path.join(script_dir, './mia-result')),
+        help='Directory for results.'
+    )
+
+    parser.add_argument(
+        '--data_atlas_dir',
+        type=str,
+        default=os.path.normpath(os.path.join(script_dir, '../data/atlas')),
+        help='Directory with atlas data.'
+    )
+
+    parser.add_argument(
+        '--data_train_dir',
+        type=str,
+        default=os.path.normpath(os.path.join(script_dir, '../data/train/')),
+        help='Directory with training data.'
+    )
+
+    parser.add_argument(
+        '--data_test_dir',
+        type=str,
+        default=os.path.normpath(os.path.join(script_dir, '../data/test/')),
+        help='Directory with testing data.'
+    )
+
+    FLAGS, unparsed = parser.parse_known_args()
+    app.run(main=main, argv=[sys.argv[0]] + unparsed)
