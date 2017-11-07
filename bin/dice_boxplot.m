fc902cb882bbd246db7b43af80582f20f63d6765
mrunibe <michael.rebsamen@students.unibe.ch>
2017-11-07 15:33:28 +0100

    Boxplot for dice coefficients from results.csv for full run with 70 training samples

---
+++ bin/dice_boxplot.m
@@ -0,0 +1,58 @@
+clearvars; clearvars -GLOBAL; close all;
+
+ALGOS = {'DF', 'kNN', 'SVM',  'SGD'};
+
+h = figure();
+N=size(ALGOS, 2);
+for idx=1:N
+    csv = strcat('../results_', ALGOS{idx}, '.csv');
+    subplot(1, N, idx);
+    plot_dice(ALGOS{idx}, csv, idx == 1)
+end
+
+set(h, 'PaperUnits', 'inches');
+set(h, 'PaperSize', [12 8]);
+set(h, 'PaperPositionMode', 'manual');
+set(h, 'PaperPosition', [0 0 12 8]);
+%set(h, 'renderer', 'painters');
+print(h, 'boxplot', '-depsc')
+
+function plot_dice(name, results_file, show_y_label)
+    csv = readtable(results_file);
+
+    WM = [];
+    GM = [];
+    V = [];
+
+    for i=1:size(csv, 1)
+       if endsWith(csv(i,:).ID, '-PP')
+          if(strcmp(csv(i,:).LABEL,'WhiteMatter'))
+              WM = [WM csv(i,:).DICE];
+          end
+          if(strcmp(csv(i,:).LABEL,'GreyMatter'))
+              GM = [GM csv(i,:).DICE];
+          end
+          if(strcmp(csv(i,:).LABEL,'Ventricles'))
+              V = [V csv(i,:).DICE];
+          end
+       end
+    end
+
+    scatter(rand(size(WM))/5+0.48, WM, 14, [0.4, 0.4, 0.4], 'o')
+    xlim([0, 2]);
+    hold on
+    scatter(rand(size(GM))/5+1.48, GM, 14, [0.4, 0.4, 0.4], 'o', 'MarkerEdgeColor',[.7 .7 .7], 'MarkerFaceColor',[.7 .7 .7])
+    scatter(rand(size(V))/5+2.48, V, 14, [0.4, 0.4, 0.4], 'o', 'MarkerEdgeColor',[0 0 .8], 'MarkerFaceColor',[0 0 .8])
+    boxplot([WM; GM; V]', {'WM', 'GM', 'V'}, 'Symbol', '')
+    if show_y_label
+        ylabel('Dice Coefficient');
+    else
+        set(gca, 'YTickLabel', []);
+    end
+    ylim([0, 1]);
+    xlim([0, 4]);
+    title(name)
+end
+
+
+    
